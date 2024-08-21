c356be0d8b601e53d8a32f9e36453ec3fd3f2ccc
Minhtuan2405 <tuannguyen.02042005@gmail.com>
2024-08-21 22:52:31 +0700

    second time

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
-const express = require ('express')
-const app = express ()
+const express = require('express')
+const app = express()
 const port = 3000
 
 app.set ("views", "./views")
 app.set ("view engine", "pug")
 
 
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
+app.get ("/product", (resquest, response) => {
+
+})
